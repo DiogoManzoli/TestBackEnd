7b0e553d058040244850421da8ef43fd3b7738f6
diogomanzoli <diogomanzoli.o@gmail.com>
2024-01-05 18:51:20 -0300

    NavBar

--- src/Controller/InvestmentsController.php
+++ src/Controller/InvestmentsController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class InvestmentsController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/investments', name: 'app_investments')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('investments/index.html.twig', [
+            'controller_name' => 'InvestmentsController',
         ]);
     }
 }
